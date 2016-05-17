b8066b770af0fc9872699451d4ae942902f27e84
Benjamin Morel <benjamin.morel@gmail.com>
2016-05-17 13:53:30 +0200

    Initial import

---
+++ bin/parallel.php
@@ -0,0 +1,66 @@
+<?php
+
+use Symfony\Component\Process\Process;
+
+$autoloadFiles = [
+    __DIR__ . '/../vendor/autoload.php', // for development
+    __DIR__ . '/../../../autoload.php'   // when installed as a composer package
+];
+
+foreach ($autoloadFiles as $autoloadFile) {
+    if (file_exists($autoloadFile)) {
+        require_once $autoloadFile;
+        break;
+    }
+}
+
+$script = array_shift($argv);
+
+if ($argc < 4) {
+    printf('Usage: %s rate concurrency command' . PHP_EOL, $script);
+    echo '  rate         The number of executions per second', PHP_EOL;
+    echo '  concurrency  The maximum number of concurrent processes', PHP_EOL;
+    echo '  command      The command to execute', PHP_EOL;
+    exit(1);
+}
+
+$rate = array_shift($argv);
+$concurrency = array_shift($argv);
+
+$command = implode(' ', $argv);
+
+$sleepTime = (int) (1000000 / $rate);
+
+$processes = [];
+
+$filter = function(Process $process) {
+    return $process->isRunning();
+};
+
+for (;;) {
+    $time = microtime(true);
+    $processes = array_values(array_filter($processes, $filter));
+
+    if (count($processes) < $concurrency) {
+        $process = new Process($command);
+        $process->start(function($type, $data) {
+            if ($type === Process::OUT) {
+                fwrite(STDOUT, $data);
+            } elseif ($type === Process::ERR) {
+                fwrite(STDERR, $data);
+            } else {
+                echo 'Unknown output type ', $type, PHP_EOL;
+                exit(1);
+            }
+        });
+
+        $processes[] = $process;
+    }
+
+    $microsecondsSpent = (int) (1000000 * (microtime(true) - $time));
+    $microsecondsSleep = $sleepTime - $microsecondsSpent;
+
+    if ($microsecondsSleep > 0) {
+        usleep($microsecondsSleep);
+    }
+}
